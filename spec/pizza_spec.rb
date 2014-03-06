a4f3b8768e62dc17a1b17ae9c475e2929d5b51b5
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-06 11:24:54 -0600

    added test for topping veggie status

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -7,14 +7,20 @@ describe Pizza do
 end
 
 describe Topping do
-  it "exists" do
-    expect(Topping).to be_a(Class)
-  end
-
   describe '.initialize' do
     it 'sets the name of the topping' do
       topping = Topping.new('olives')
       expect(topping.name).to eq('olives')
     end
   end
+
+  it "exists" do
+    expect(Topping).to be_a(Class)
+  end
+
+  it "sets whether the topping is vegetarian" do
+    topping = Topping.new('bell peppers', vegetarian = true)
+    expect(topping.vegetarian).to eq(true)
+  end
+
 end
